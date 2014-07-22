db7d65b4db8d69e5eb8a44e0671f14641a9ac088
Lucas Polonio <polonio.lucas@gmail.com>
2014-07-22 01:01:06 -0300

    refactoring

--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
 require 'rubygems' if RUBY_VERSION < "1.9"
 require 'sinatra/base'
 
-class MyApp < Sinatra::Base
+class Lucas < Sinatra::Base
   get '/' do
     @test = 'Hello!'
     erb :index
   end
 end
 
-MyApp.run!
+Lucas.run!
